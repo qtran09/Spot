5324703bf1b36783313421ebb66db215569d84f8
Oliver Pang <opang23@gmail.com>
2017-11-04 10:58:57 -0400

    Working Location + Started path

--- SearchParty/app/src/main/java/com/sp/searchparty/SearchMap.java
+++ SearchParty/app/src/main/java/com/sp/searchparty/SearchMap.java
@@ -1,13 +1,14 @@
 package com.sp.searchparty;
 
 import android.Manifest;
+import java.util.List;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
-import android.location.Location;
+//import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -47,6 +48,7 @@ import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.FusedLocationProviderApi;
 import com.google.android.gms.tasks.Task;
 import android.location.Location;
 
@@ -79,7 +81,7 @@ public class SearchMap extends FragmentActivity implements OnMapReadyCallback, G
                     .build();
         }mGoogleApiClient.connect();
         mLocationRequest = createLocationRequest();
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        //mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
     }
 
 
@@ -110,13 +112,15 @@ public class SearchMap extends FragmentActivity implements OnMapReadyCallback, G
                 Log.i("onResult","SearchMap Connected " + status.getStatusCode());
                 switch (status.getStatusCode()) {
                     case LocationSettingsStatusCodes.SUCCESS:
-                        drawLines();
+                        //mMap.setMyLocationEnabled(true);
+                        startLocationUpdates();
                         break;
                     case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
                         ActivityCompat.requestPermissions(SearchMap.this,
                                 new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
                                 1);
-                        drawLines();
+                        //mMap.setMyLocationEnabled(true);
+                        startLocationUpdates();
                         break;
                     case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
                         break;
@@ -124,11 +128,19 @@ public class SearchMap extends FragmentActivity implements OnMapReadyCallback, G
             }
         });
     }
-    public void drawLines(){
+    public void startLocationUpdates(){
         Log.i("drawLines","drawLines");
         Log.i("drawLines",""+(ContextCompat.checkSelfPermission(SearchMap.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED));
 
         if (ContextCompat.checkSelfPermission(SearchMap.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            Log.i("drawLines","requestingupdates");
+//            LocationServices.FusedLocationApi.setMockMode(mGoogleApiClient, true);
+//            Location mockloc = new Location("");
+//            mockloc.setLatitude(0.0d);
+//            mockloc.setLongitude(1.1d);
+//            LocationServices.FusedLocationApi.setMockLocation(mGoogleApiClient, mockloc);
+
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
 
 //            mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
 //            Log.i("onLocationChanged",""+mLastLocation);
@@ -137,19 +149,34 @@ public class SearchMap extends FragmentActivity implements OnMapReadyCallback, G
 //                Log.i("onLocationChanged",String.valueOf(mLastLocation.getLatitude()));
 //                Log.i("onLocationChanged",String.valueOf(mLastLocation.getLongitude()));
 //            }
-
-            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
-
         }
     }
 
     @Override
     public void onLocationChanged(Location location) {
         mLastLocation = location;
-        Log.i("onLocationChanged",String.valueOf(mLastLocation.getLatitude()));
-        Log.i("onLocationChanged",String.valueOf(mLastLocation.getLongitude()));
+        Log.i("onLocationChangedLat",String.valueOf(mLastLocation.getLatitude()));
+        Log.i("onLocationChangedLong",String.valueOf(mLastLocation.getLongitude()));
+//        mMap.addCircle(new CircleOptions()
+//                .center(new LatLng(37.4, -122.1))
+//                .radius(1000)
+//                .strokeWidth(10)
+//                .strokeColor(Color.GREEN)
+//                .fillColor(Color.argb(128, 255, 0, 0))
+//                .clickable(true));
     }
 
+    private void drawCircles(List<LatLng> Positions){
+        for(LatLng L : Positions){
+            mMap.addCircle(new CircleOptions()
+                    .center(new LatLng(37.4, -122.1))
+                    .radius(1000)
+                    .strokeWidth(10)
+                    .strokeColor(Color.GREEN)
+                    .fillColor(Color.argb(80, 255, 0, 255/Positions.indexOf(L)))
+                    .clickable(true));
+        }
+    }
 
 
     @Override
@@ -160,7 +187,6 @@ public class SearchMap extends FragmentActivity implements OnMapReadyCallback, G
                 // If request is cancelled, the result arrays are empty.
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    drawLines();
                 } else {
                     System.exit(0);
                 }
@@ -171,11 +197,11 @@ public class SearchMap extends FragmentActivity implements OnMapReadyCallback, G
     public void onConnectionSuspended(int i) {
     }
     protected LocationRequest createLocationRequest() {
-        LocationRequest mLocationRequest = new LocationRequest();
-        mLocationRequest.setInterval(5000);
-        mLocationRequest.setFastestInterval(3000);
-        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-        return mLocationRequest;
+        LocationRequest temp = new LocationRequest();
+        temp.setInterval(5000);
+        temp.setFastestInterval(3000);
+        temp.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        return temp;
     }
 
 
